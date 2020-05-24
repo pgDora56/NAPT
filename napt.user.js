4244eafa0b6fbe356c2a13080bd5d53612551a17
Dora F <doradora.prog@gmail.com>
2020-05-25 05:17:08 +0900

    Release 1.4.0

--- napt.user.js
+++ napt.user.js
@@ -1,7 +1,7 @@
 /// ==UserScript==
 // @name         NAPT - NQA2 AutoPaste & Send Tool
 // @namespace    https://github.com/pgDora56
-// @version      1.3.0
+// @version      1.4.0
 // @description  Auto paste for NQA2's chat-area & send, and more
 // @author   Dora F.
 // @match    https://powami.herokuapp.com/nqa2/*
@@ -10,6 +10,8 @@
 // @supportURL	https://github.com/pgDora56/NAPT/issues
 // @updateURL	https://raw.githubusercontent.com/pgDora56/NAPT/master/napt.user.js
 // ==/UserScript==
+
+var debug_mode = true;
 
 var i, movelink;
 var hotkeyBox = null;
@@ -22,7 +24,8 @@ var flavors;
 
 //
 // Provider setting
-var isProvider = window.location.href.slice(-8) == "provider";
+var isProvider = (window.location.href.slice(-8) == "provider" || window.location.href.slice(-9) == "provider?" );
+var displayRank = window.location.href.slice(-1) == "?";
 var barcolor = isProvider ? "sienna" : "darkblue";
 
 //
@@ -32,16 +35,19 @@ var wb = document.getElementById("wrong-button");
 var tb = document.getElementById("through-button")
 var consoleApply = config.querySelector(".ui.positive.right.submit.button");
 var chatbox = document.querySelector(".chat-box");
+var score = [];
 
 initialization();
 
 var INPUTS = ['INPUT', 'TEXTAREA'];
 document.addEventListener('keydown', function (e) {
     // Key Down
-    if (INPUTS.indexOf(e.target.tagName) == -1) { // Do not process when input texts
+    if (INPUTS.indexOf(e.target.tagName) == -1) { // Do not process when input text
+        updateScoreData(); // なんか押されたらRankを更新させる
         var pressed = String.fromCharCode(e.which).toLowerCase();
         var keycode = e.which;
         pressed = (e.altKey ? 'A' : '') + (e.shiftKey ? 'S' : '') + pressed;
+        if(debug_mode) console.log("Key Press: " + pressed + ", Key Code: " + keycode);
         var num = keycode - 48;
 
         if(num >= 0 && num <= 2) {
@@ -80,6 +86,12 @@ document.addEventListener('keydown', function (e) {
                 else chat(match[0].content);
             }
         }
+//         if(pressed == "z"){
+//             for(i = 0; i < 41700; i++) {
+//                 chat("モチョカワイイネー");
+//             }
+//             chat("(*>△<)");
+//         }
 
         if(isProvider){
             if(pressed == "q"){
@@ -111,12 +123,12 @@ document.addEventListener('keydown', function (e) {
                     freezeplus.click();
                 }
             }
-            // else if(pressed == "z") {
-            //     var wroplus = document.querySelector(".player.selected").querySelector(".wrong-plus.ui.icon.red.button");
-            //     for(i = 0; i < 41700; i++) {
-            //         wroplus.click();
-            //     }
-            // }
+//             else if(pressed == "z") {
+//                 var wroplus = document.querySelector(".player.selected").querySelector(".wrong-plus.ui.icon.red.button");
+//                 for(i = 0; i < 41700; i++) {
+//                     wroplus.click();
+//                 }
+//              }
         }
     }
 }, false);
@@ -131,6 +143,11 @@ function initialization() {
 
     config.querySelector(".fields").insertAdjacentHTML('beforeend', '<div class="field"><label>flavor-sub1</label><input type="text" id="flavor1" maxlength="10"></div>'
                                                       +'<div class="field"><label>flavor-sub2</label><input type="text" id="flavor2" maxlength="10"></div>')
+    //document.querySelector(".five.wide.column").insertAdjacentHTML('beforeend', '<div id="rank"></div>');
+    if(displayRank){
+        document.querySelector(".game-view.ui.divided.grid").insertAdjacentHTML('afterbegin', '<div class="two wide column"><div id="rank"></div></div>');
+        document.querySelector(".eleven.wide.column").setAttribute("class", "nine wide column");
+    }
     hotkeyBox = document.getElementById("napt-hotkey");
     importHotkey();
     refreshHotkey();
@@ -138,13 +155,15 @@ function initialization() {
     document.querySelector(".ui.dividing.header").style.display = "none";
     document.querySelector(".page-header").style = "background-color: " + barcolor + ";";
 
+    var link = window.location.href;
+    if(displayRank) link = link.slice(0,-1);
     if(isProvider){
-        movelink = window.location.href.slice(0, -8) + "player";
+        movelink = link.slice(0, -8) + "player";
     }
     else{
-        movelink = window.location.href.slice(0, -6) + "provider";
+        movelink = link.slice(0, -6) + "provider";
     }
-    document.querySelector("h1").innerHTML = "<a href='" + movelink + "'>" + "Nagaya Quiz Arena 2 with NAPT</a>";
+    document.querySelector("h1").innerHTML = "<a href='" + movelink + (displayRank ? "?" : "" ) + "'>" + "Nagaya Quiz Arena 2 with NAPT</a>";
     cb.innerHTML = "正解(Q)";
     wb.innerHTML = "誤答(W)";
     tb.innerHTML = "スルー(E)";
@@ -252,6 +271,49 @@ function flavorSwap(id){
     config.querySelector(".name").value = flavors[0];
     config.querySelector(".flavor").value = flavors[1];
     consoleApply.click();
+}
+
+function updateScoreData(){
+    if(!displayRank) return;
+    var players = document.getElementById("players").getElementsByClassName('player');
+    console.log(players.length);
+    score = [];
+
+    // playerをすべて取得
+    for(i=0;i<players.length;i++){
+        var n = players[i].getElementsByClassName("name");
+        var scores = players[i].getElementsByClassName("score-value");
+        score.push([0,n[0].textContent, scores[0].textContent, scores[1].textContent]);
+    }
+
+    // Sort(丸数→罰数)
+    score.sort(function(a,b){
+        if(a[2] > b[2]) return -1;
+        if(a[2] < b[2]) return 1;
+        if(a[3] < b[3]) return -1;
+        if(a[3] > b[3]) return 1;
+        return 0;
+    });
+
+    // 順位を修正
+    var rank = 1;
+    score[0][0] = 1;
+    for(i=1;i<score.length;i++){
+       if(score[i-1][2] != score[i][2] || score[i-1][3] != score[i][3]){
+           rank = i + 1;
+       }
+       score[i][0] = rank;
+    }
+
+    // HTML生成
+    var text = '<table>';
+    for(i=0;i<score.length;i++){
+        text += "<tr><td>" + score[i][0] + ".</td><td>" + score[i][1] + "</td><td>:" + score[i][2] + "</td><td>-</td><td>" + score[i][3] + "</td></tr>";
+    }
+    text += "</table>";
+
+    // HTML更新
+    document.getElementById("rank").innerHTML = text;
 }
 
 // Data Translate to local(for hotkey settings) ========================================================
